ab4e104ba4601ac1fe7f60be6eac638b8575ccd3
suraj gawas <suraj gawas>
2016-08-19 20:35:09 -0700

    0819_4

--- src/test/java/auto/test.java
+++ src/test/java/auto/test.java
@@ -3,6 +3,7 @@ package auto;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.Test;
 
@@ -26,8 +27,8 @@ public class test {
 
 	@Test
 	public void doLogin(){
-		System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
-		WebDriver driver = new ChromeDriver();
+		//System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
+		WebDriver driver = new FirefoxDriver();
 		driver.get("https://gmail.com");
 		//driver.findElement(By.xpath("//*[@id='gmail-sign-in']")).click();
 		driver.findElement(By.xpath("//*[@id='Email']")).sendKeys("suraj@gmail.com");
